dd40e5f31d0f792354b43bf56276107bf2eb3e1f
Maxim Ryndin <rgmasim@gmail.com>
2017-01-13 21:55:15 +0300

    Initial commit.

---
+++ index.js
@@ -0,0 +1,15 @@
+let http = require('http');
+let url = require('url');
+
+var server = http.createServer( (req, res) => {
+	let uri = url.parse(req.url, true)
+	let timeout = uri.query.timeout || uri.query.t || 500
+	setTimeout(function(){
+		res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'})
+    	res.end('{"status":"ok"}')
+	}, timeout)
+});
+
+server.listen(4000, function() {
+	console.log("Listening 4000...")
+});
